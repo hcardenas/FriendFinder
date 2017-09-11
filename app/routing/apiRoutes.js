4f4aabac2f421c0d30dccfa6471481503e5c6020
Helmut Cardenas <aleksey89col@gmail.com>
2017-09-11 12:51:35 -0400

    done

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,8 +7,25 @@ module.exports = function(app) {
 	});
 
 	app.post('/api/friends', function(req, res) {
-		friendsArr.push(req.body);
-		console.log(`pushed object ${req.body}`);
+		var user = req.body;
+		var userScores = user.scores;
+
+		var bestMatch = friendsArr[0];
+		var bestScore = 100;
+		var compScore = 0;
+		for (var i in friendsArr) {
+			for (var j = 0 ; j < 10; ++j) {
+				compScore += Math.abs(friendsArr[i].scores[j] - userScores[j]);
+			}
+			if (bestScore > compScore) {
+				bestMatch = friendsArr[i];
+				bestScore = compScore;
+			}
+			compScore = 0;
+		}
+
+		res.json(bestMatch);
+		friendsArr.push(user);
 	})
 
 }
